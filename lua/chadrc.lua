620c1e230d91861658ba0b1c34ff3d835202dae1
feel2code <feel2code@proton.me>
2025-05-15 02:06:47 +0300

    git blame and pylint configured

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,17 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+  theme = "onedark",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 -- M.nvdash = { load_on_startup = true }
@@ -20,5 +20,13 @@ M.base46 = {
 --          lazyload = false
 --      }
 --}
+
+vim.opt.relativenumber = true
+
+vim.keymap.set('i', '<C-y>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
 
 return M
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,5 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map("n", "gb", "<cmd> G blame <cr>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
